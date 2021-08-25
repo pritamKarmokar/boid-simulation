e4e45c81dfb3a8e7c2ee5a87a56e9f38bbd7ec09
pritam <pritam.karmokar@gmail.com>
2021-08-24 20:10:37 -0500

    Experiment wall forces

--- boids.py
+++ boids.py
@@ -11,7 +11,9 @@ import numpy as np
 SCREEN_SIZE = WIDTH, HEIGHT = (1024, 1024)
 SCREEN_CENTER = (WIDTH//2, HEIGHT//2)
 SCREEN_BG_COLOR = (40,40,40)
-NUM_BOIDS = 10
+NUM_BOIDS = 3
+FPS = 30
+
 
 class Boid(pygame.sprite.Sprite):
     def __init__(self, simulator):
@@ -29,25 +31,53 @@ class Boid(pygame.sprite.Sprite):
         
         # appearance
         self.color = choice(multi_colors)
-        self.image = pygame.Surface((15,15))
-        self.rect = pygame.draw.polygon(self.image, self.color, ((0, 5), (0, 10), (10, 10), (10, 15), (15, 8), (10, 0), (10, 5)))
-        # self.image.fill(self.color)
+
+        # shape
         self.spawn_pad = 50
         self.position = pygame.Vector2(randint(self.spawn_pad, WIDTH-self.spawn_pad), randint(self.spawn_pad, HEIGHT-self.spawn_pad))
+
+        # kinematics
         self.acceleration = pygame.Vector2(0.0,0.0)
-        self.velocity = pygame.Vector2(randint(-5,5),randint(-5,5)) * 10
+        self.velocity = pygame.Vector2(randint(-5,5),randint(-5,5)) * 20
         self.angle = atan2(self.velocity[1], self.velocity[0])
-        # self.rect = self.image.get_rect()
+
+        # perception
+        self.perception = randint(20,30)
+
+        self.max_speed = randint(50,100)
+        self.max_force = randint(5,8)
+
+
+    def wrap_horizontally(self, x):
+        # if x <= 0:
+        #     x = WIDTH
+        # elif x >= WIDTH:
+        #     x = 0
+        
+        return x%WIDTH
+
+
+    def wrap_vertically(self, y):
+        # if y <= 0:
+        #     y = HEIGHT
+        # elif y >= HEIGHT:
+        #     y = 0
+        return y%HEIGHT
+
 
     def update(self):
+        self.position[0] = self.wrap_horizontally(int(self.position[0]))
+        self.position[1] = self.wrap_vertically(int(self.position[1]))
+        self.rect.centerx = self.position[0]
+        self.rect.centery = self.position[1]
 
-        self.rect.centerx = int(self.position[0])
-        self.rect.centery = int(self.position[1])
 
     def update_kinematics(self,dt):
-        self.acceleration = pygame.Vector2(random()*4 - 2, random()*4 - 2)
+        # self.update_acceleration()
+        # self.acceleration = pygame.Vector2(random()*4 - 2, random()*4 - 2)
         self.velocity += self.acceleration*dt
         self.position += self.velocity*dt
+        self.angle = atan2(self.velocity[1], self.velocity[0])
 
     
     def draw_boid(self, screen, position=None, angle=None):
@@ -64,25 +94,13 @@ class Boid(pygame.sprite.Sprite):
 
 
 
-
-
-
-
-
 class BoidSimulator:
     def __init__(self):
+        # create boids and assign let them assign themselves to a group
         self.boid_sprites = pygame.sprite.Group()
         self.boids = []
         for _ in range(NUM_BOIDS):
             self.boids.append(Boid(self))
-
-        # dist = 50
-        # deltas = [(0,0), 
-        #           (dist,-dist), (-dist,-dist), (-dist,dist), (dist,dist),
-        #           (2*dist,-2*dist), (-2*dist,-2*dist), (-2*dist,2*dist), (2*dist,2*dist), (3*dist, 0)]
-        # positions = [pygame.Vector2(SCREEN_CENTER) + delta for delta in deltas]
-        # for i, position in enumerate(positions):
-        #     self.boids[i].position = pygame.Vector2(position)
 
 
 
@@ -96,24 +114,109 @@ class BoidSimulator:
         clock = pygame.time.Clock()
 
         sim_running = True
-        FPS = 30
+        
         while sim_running:
-
+            # update clock
             dt = clock.tick(FPS) / 1000
+
+            # re-draw background
             screen.fill(SCREEN_BG_COLOR)
 
+            # handle events (only quit for now)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sim_running = False
                     pygame.quit()
 
+            # draw 
             for sprite in self.boid_sprites:
                 sprite.draw_boid(screen)
-            self.boid_sprites.update()
 
+            # update acceleration
+            for boid in self.boid_sprites:
+                avg_vel = np.zeros((1,2))
+                com = np.zeros((1,2))
+                alignment = np.zeros((1,2))
+                cohesion = np.zeros((1,2))
+                separation = np.zeros((1,2))
+                avg_v = np.zeros((1,2))
+                avg_wall = np.zeros((1,2))
+                count = 0
+                # boid alignment
+                for sprite in self.boid_sprites:
+                    dist = np.linalg.norm(sprite.position-boid.position)
+                    if sprite is not boid and dist < boid.perception:
+                        avg_vel += sprite.velocity
+                        count += 1
+                if count > 0:
+                    avg_vel /= count
+                    if np.linalg.norm(avg_vel) > 0:
+                        avg_vel = (avg_vel/np.linalg.norm(avg_vel)) * boid.max_speed
+                    alignment = avg_vel - boid.velocity
+                    if np.linalg.norm(alignment) > 0:
+                        alignment = (alignment/np.linalg.norm(alignment)) * boid.max_force
+
+                count = 0
+                # boid cohesion
+                for sprite in self.boid_sprites:
+                    dist = np.linalg.norm(sprite.position-boid.position)
+                    if sprite is not boid and dist < boid.perception:
+                        com += sprite.position
+                        count += 1
+                if count > 0:
+                    com /= count
+                    vec_com = com - boid.position
+                    if np.linalg.norm(vec_com) > 0:
+                        vec_com = (vec_com/np.linalg.norm(vec_com)) * boid.max_speed
+                    cohesion = vec_com - boid.velocity 
+                    if np.linalg.norm(cohesion) > 0:
+                        cohesion = (cohesion/np.linalg.norm(cohesion)) * boid.max_force
+
+                count = 0
+                # boid separation
+                for sprite in self.boid_sprites:
+                    dist = np.linalg.norm(sprite.position-boid.position)
+                    if sprite is not boid and dist < boid.perception:
+                        diff = (boid.position - sprite.position) / dist
+                        avg_v += diff
+                        count += 1
+                if count > 0:
+                    avg_v /= count
+                walls = [pygame.Vector2(boid.position[0],0), 
+                         pygame.Vector2(0,boid.position[1]),
+                         pygame.Vector2(boid.position[0],HEIGHT),
+                         pygame.Vector2(WIDTH,boid.position[1])]
+                wall_count = 0
+                for wall in walls:
+                    dist = np.linalg.norm(wall-boid.position)
+                    if dist < boid.perception*5:
+                        diff = (boid.position - wall) / dist**2
+                        avg_wall += diff
+                        wall_count += 1
+                if count > 0 or wall_count > 0:
+                    avg_v += 100*avg_wall/wall_count
+                    if np.linalg.norm(avg_v) > 0:
+                        avg_v = (avg_v/np.linalg.norm(avg_v)) * boid.max_speed
+                    separation = avg_v - boid.velocity
+                    if np.linalg.norm(separation) > 0:
+                        separation = (separation/np.linalg.norm(separation)) * boid.max_force
+
+                acc_total = 1*alignment.flatten() + 1*cohesion.flatten() + 1*separation.flatten()
+                
+                # update boid acceleration
+                boid.acceleration = pygame.Vector2(acc_total[0], acc_total[1])
+
+
+
+                # separation = (sum([(np.linalg.norm(nbr.position - boid.position))**-2 for nbr in self.boid_sprites if nbr is not boid]) / (NUM_BOIDS-1) ) - (np.linalg.norm(boid.position))**-2
+                # print(f'{alignment}, {cohesion}, {separation}, {acc_total}')
+
+            # update kinematics
             for boid in self.boid_sprites:
                 boid.update_kinematics(dt)
-                # print(f'dt-{dt}, boid_position-{boid.position[0]},{boid.position[1]}, rect-{boid.rect.centerx},{boid.rect.centery}')
+
+            # update rects
+            self.boid_sprites.update()
 
             pygame.display.flip()
 
